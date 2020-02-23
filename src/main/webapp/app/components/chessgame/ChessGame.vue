eff523a25083139839aa50021c0ba56c852a6786
Robert Yokota <rayokota@gmail.com>
2020-02-22 23:48:12 -0800

    Minor fix

--- src/main/webapp/app/components/chessgame/ChessGame.vue
+++ src/main/webapp/app/components/chessgame/ChessGame.vue
@@ -38,6 +38,7 @@ export default {
                     color: this.toColor(),
                     dests: this.possibleMoves(),
                 },
+                orientation: this.orientation,
             })
         },
     }
